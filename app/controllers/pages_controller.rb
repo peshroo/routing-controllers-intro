a0b4e149aaa92dea89b6a8e6249decf8d8e72d42
Roopesh <rrattan.89@gmail.com>
2017-06-12 19:31:38 -0400

    instance variables for headers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "W E L C O M E"
+  end
 
+  def about
+    @header = "A B O U T"
+  end
+
+  def contest
+    @header = "C O N T E S T"
   end
 
 end
