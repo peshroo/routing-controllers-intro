cc49037293d36b8bcba1525d99e5a215b8ccfb2e
Roopesh <rrattan.89@gmail.com>
2017-06-12 19:18:22 -0400

    defining route for about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get '/' => 'pages#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'pages#welcome'
+
+  get '/welcome' => 'pages#welcome'
+
+  get '/about' => 'pages#about'
+
 end
